4a915da56327c0d971d94a12732335273d0e5640
Roman <romabulenkov@gmail.com>
2025-11-07 21:53:59 +0300

    _
    :}

--- gode 2 good luck.py
+++ gode 2 good luck.py
@@ -1,2 +1,33 @@
-print(1-9)
-print(1-9)
+import arcade
+import random
+
+SCREEN_WIDTH = 3440
+SCREEN_HEIGHT = 1440
+SCREEN_TITLE = "sigmaboy"
+
+
+class Rom4ik(arcade.Window):
+    def __init__(self):
+        super().__init__(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, title=SCREEN_TITLE, update_rate=1 / 164.999, fullscreen=True)
+
+    def on_mouse_motion(self, x: int, y: int, dx: int, dy: int) :
+        pass
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        pass
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        pass
+
+    def on_draw(self):
+        arcade.start_render()
+
+    def update(self, delta_time):
+        pass
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        pass
+
+
+window = Rom4ik()
+arcade.run()
